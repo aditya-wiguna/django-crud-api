678c21d9f88066be2f8b1c8e178b379b76d01245
I Wayan Aditya Wiguna <adityawiguna0@gmail.com>
2020-08-28 14:23:41 +0800

    add validation

--- customer/serializers.py
+++ customer/serializers.py
@@ -3,12 +3,19 @@ from customer.models import Customer
 
 
 class CustomerSerializer(serializers.ModelSerializer):
+    def validate(self, data):
+        """
+        Check that the start is before the stop.
+        """
+        if data['name'] == "":
+            raise serializers.ValidationError("name is required")
+        return data
     class Meta:
         model = Customer
         fields = ('id',
                   'name',
                   'marital_status',
-                  'identity_number',\
+                  'identity_number',
                   'address',
                   'parent_name',
                   'is_active'
