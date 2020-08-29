04557923c8d91f7f3d19b03ebae5966e4b2ad24b
Aditya Wiguna <adityawiguna0@gmail.com>
2020-08-29 12:55:09 +0800

    add cors

--- apiCustomer/settings.py
+++ apiCustomer/settings.py
@@ -28,7 +28,8 @@ SECRET_KEY = 'b!in(#283i(au^mq_wh=6#^dusy*sf5cd8+96a2i+o*ak#v3$j'
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    'api-customer-wiguna.herokuapp.com'
+    'api-customer-wiguna.herokuapp.com',
+    '127.0.0.1'
 ]
 
 
@@ -42,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'customer.apps.CustomerConfig'
 ]
 
@@ -53,6 +55,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
 
 ROOT_URLCONF = 'apiCustomer.urls'
@@ -134,3 +138,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CORS_ORIGIN_ALLOW_ALL = True
